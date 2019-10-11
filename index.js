de1c252e224178c0c7b6f624d4fc7818ba775a7d
苏鹏飞 <supengfei@supengfeideMacBook-Pro.local>
2019-10-11 17:17:53 +0800

    角度调整与配置暴露

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 !(function (e) {
     const GroupHeadImg = (dom, options = {}) => {
-        const { width = '10rem', images = [] } = options,
+        const { width = '50px', images = [], upperLimit = 10, haveAnimation = true, boxStyle = {}, itemStyle = {} } = options,
             widthVal = parseFloat(width),
             unitVal = width.replace(`${widthVal}`, ''),
             length = images.length,
@@ -19,9 +19,9 @@
                         break
                     }
                     default: {
-                        index = total - 1 - index;
+                        index = total - 1 - index;//保证第一个在图层最上面
                         let itemDeg = 360 / total,
-                            reviseDeg = 0,//矫正值
+                            reviseDeg = total == 2 ? 0 : 90,//矫正值
                             resDeg = itemDeg * index - reviseDeg,
                             itemHd = resDeg / 180 * Math.PI,
                             cosVal = Math.cos(itemHd) * translateScale + unitVal,
@@ -41,16 +41,14 @@
                     borderWidth,
                     translateScale = 1;//每个图片边框大小
                 const l = arr.length,
-                    setVal = (scale,borderWidthScale=10) => {
-                        let tsfmVal = widthVal * 0.5 * 0.5;
-
+                    setVal = (scale, borderWidthScale = 10) => {
                         imgItemWidthScale = scale;
                         imgItemWidth = (widthVal) / 2 * imgItemWidthScale + unitVal;
                         imgItemWidthVal = parseFloat(imgItemWidth);
                         borderWidth = imgItemWidthVal / borderWidthScale + unitVal;
-                        translateScale = (widthVal - imgItemWidthVal) /2;
+                        translateScale = (widthVal - imgItemWidthVal) / 2;
 
-                        console.log(imgItemWidthVal, scale, translateScale)
+                        // console.log(imgItemWidthVal, scale, translateScale)
                     };
 
                 switch (l) {
@@ -60,19 +58,19 @@
                         break
                     }
                     case 2: {
-                        setVal(1.2,20);
+                        setVal(1.2, 20);
                         break
                     }
                     case 3: {
-                        setVal(1.1,15);
+                        setVal(1.1, 15);
                         break
                     }
                     case 4: {
-                        setVal(0.95,12);
+                        setVal(1, 12);
                         break
                     }
                     default: {
-                        setVal(0.95,12);
+                        setVal(0.98, 12);
                         break
                     }
                 }
@@ -81,6 +79,9 @@
                     const div = document.createElement('div'),
                         divChild = document.createElement('div');
                     div.className = `groupHeadImgItem-${i}`;
+
+                    div.appendChild(divChild);
+                    dom.appendChild(div);
                     addStyleAttr(div, {
                         position: 'absolute',
                         top: '50%',
@@ -88,32 +89,41 @@
                         transform: 'translate(-50%,-50%)'
                     })
                     addStyleAttr(divChild, {
+                        transform: haveAnimation ? undefined :calcPositon(i, l, translateScale) ,
+                        transition: haveAnimation ?'all 1000ms':undefined,
+                        backgroundColor: '#fff',
+                        // boxShadow: `0 0 ${borderWidth} #000`,
+                        ...itemStyle,
                         position: 'relative',
-                        transform: calcPositon(i, l, translateScale),
                         width: imgItemWidth,
                         height: imgItemWidth,
                         borderRadius: '50%',
-                        backgroundColor: '#f4f4f4',
                         backgroundImage: `url(${arr[i]})`,
                         backgroundPosition: 'center',
                         backgroundSize: 'contain',
                         backgroundRepeat: 'no-repeat',
                         border: `${borderWidth} solid #ffffff`,
                         boxSizing: 'border-box',
-                        boxShadow: `0 0 ${borderWidth} #000`
+
                     });
-                    div.appendChild(divChild);
-                    dom.appendChild(div)
+
+                    if (haveAnimation) {
+                        setTimeout(() => {
+                            addStyleAttr(divChild, {
+                                transform: calcPositon(i, l, translateScale)
+                            })
+                        }, 0)
+                    }
                 }
             };;
         if (dom) {
             addStyleAttr(dom, {
+                ...boxStyle,
+                /*boxStyle不接受以下属性*/ 
                 position: 'relative',
                 width,
                 height: width,
                 borderRadius: '50%',
-                backgroundColor: '#f4f4f4',
-                // overflow: 'hidden',
                 boxSizing: 'border-box'
             })
         } else {
@@ -123,7 +133,7 @@
             console.warn('图片个数为0')
         } else {
             dom.innerHTML = '';//防止多次调用
-            createEle(images.slice(0, Math.min(length, 5)))
+            createEle(images.slice(0, Math.min(length, upperLimit)))
         }
 
     }
